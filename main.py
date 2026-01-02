b995950456c0e3bd1e1621beb6517dacbc180daf
traicovn <little.dusk7898@pleasedontcontactme.com>
2026-01-02 05:13:28 +0000

    complete

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
+import sys
 def get_book_text(path_to_file):
     with open(path_to_file, "r") as f:
         return f.read()
 
-from stats import count_words
+from stats import count_words, char_count, sorted_char_list
 
 def main():
-    path_to_file = "books/frankenstein.txt"
-    file_contents = get_book_text(path_to_file)
-    num_words = count_words(file_contents)
-    print(f"{num_words} words found in the document")
+    if len(sys.argv) < 2:
+       print("Usage: python3 main.py <path_to_book>")
+       sys.exit(1)
+    
+    else:
+       path_to_file = (sys.argv[1])
+       file_contents = get_book_text(path_to_file)
+       num_words = count_words(file_contents)
+       print(f"Found {num_words} total words")
+
+       char_counts = char_count(file_contents)
+       print("\nCharacter counts:")
+       print(char_counts)
+    
+       counts = char_counts
+       sorted_chars = sorted_char_list(counts)
+       for entry in sorted_chars:
+           print(f"{entry['char']}: {entry['num']}")
+       print("--- End report ---")
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,19 @@
 def count_words(text):
     words = text.split()
     return len(words)
+
+def char_count(text: str) -> dict[str, int]:
+
+    text = text.lower()
+    counts = {}
+
+    for char in text:
+        counts[char] = counts.get(char, 0) + 1
+    return counts
+
+def sorted_char_list(char_counts: dict[str, int]) -> list[dict[str, int]]:
+    char_list = [{"char": ch, "num": count}
+             for ch, count in char_counts.items() if ch.isalpha()]
+    char_list.sort(key=lambda x: x["num"], reverse=True)
+
+    return char_list
